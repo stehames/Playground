27cd18ff1b0acceecd03ada919c50c2eaccaf97f
stehames <stehh.amess@gmail.com>
2021-09-29 16:48:35 -0300

    parametros compartilhados

---
+++ 20-09/meu-app/controllers/HomeController.js
@@ -0,0 +1,13 @@
+const HomeController = {
+    index: (req, res) =>{
+
+        let info = {
+            titulo: "Digital House",
+            cursos:['Full Stack', 'Mobile Andoird', 'Marketing Digital']
+        };
+
+        return res.render('index', info);
+    }
+};
+
+module.exports = HomeController;
--- 20-09/meu-app/routes/index.js
+++ 20-09/meu-app/routes/index.js
@@ -2,14 +2,11 @@ var express = require('express');
 
 var router = express.Router();
 
+var HomeController = require('../controllers/HomeController');
 var ContatoController = require('../controllers/ContatoController');
 var EstudanteController = require('../controllers/EstudanteController');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
+router.get("/", HomeController.index);
 router.get("/contato", ContatoController.index);
 router.get("/estudantes", EstudanteController.index);
 
