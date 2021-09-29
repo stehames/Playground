c38d9849f180a405935dbd75e88a37e41a401549
stehames <stehh.amess@gmail.com>
2021-09-29 11:52:27 -0300

    primer view

--- 20-09/meu-app/routes/index.js
+++ 20-09/meu-app/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+
 var router = express.Router();
+
+var ContatoController = require('../controllers/ContatoController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get("/contato", ContatoController.index);
 
 module.exports = router;
