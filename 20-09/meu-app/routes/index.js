ec3c09aab883c4ae75a149b93ba25221baa31e76
stehames <stehh.amess@gmail.com>
2021-10-22 09:47:20 -0300

    partial files

--- 20-09/meu-app/routes/index.js
+++ 20-09/meu-app/routes/index.js
@@ -1,10 +1,10 @@
-var express = require('express');
+var express = require("express");
 
 var router = express.Router();
 
-var HomeController = require('../controllers/HomeController');
-var ContatoController = require('../controllers/ContatoController');
-var EstudanteController = require('../controllers/EstudanteController');
+var HomeController = require("../controllers/HomeController");
+var ContatoController = require("../controllers/ContatoController");
+var EstudanteController = require("../controllers/EstudanteController");
 
 router.get("/", HomeController.index);
 router.get("/contato", ContatoController.index);
