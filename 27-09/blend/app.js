4143a4ba393d5aa990a785c18210dea2c15b1e0b
stehames <stehh.amess@gmail.com>
2021-10-25 14:35:20 -0300

    Métodos PUT e DELETE

--- 27-09/blend/app.js
+++ 27-09/blend/app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var produtosRouter = require('./routes/produtos');
 
 var app = express();
 
@@ -18,9 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/produtos', produtosRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
