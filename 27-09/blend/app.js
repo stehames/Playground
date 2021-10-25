85ae3c666b9b18c2a49ee2ba30f77b181f5d1dbe
stehames <stehh.amess@gmail.com>
2021-10-25 14:37:41 -0300

    Error 404

--- 27-09/blend/app.js
+++ 27-09/blend/app.js
@@ -26,7 +26,9 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/produtos', produtosRouter);
 
-
+app.use( (req, res)=>{
+  return res.status(404).render('not-found');
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
